12069f985bd1d3455268aa3e09837b0b29f20307
Alex Piggott <alex@piggottfamily.co.uk>
2016-06-26 12:22:20 -0400

    Finished very basic state example

--- src/test/java/person/alexp/halva/examples/StateExample.java
+++ src/test/java/person/alexp/halva/examples/StateExample.java
@@ -1,5 +1,6 @@
 package person.alexp.halva.examples;
 
+import fj.P2;
 import fj.Unit;
 import fj.data.State;
 import io.soabase.halva.alias.TypeAlias;
@@ -20,16 +21,22 @@ import static io.soabase.halva.sugar.Sugar.List;
  */
 public class StateExample {
 
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    // 1) Native implementation of stack
+
     //TODO: would be nice to declare "TypeAlias Stack" inside here vs in its own compilcation unit
     //TODO: would be nice to declare "StackAlias" and get "Stack" vs the other way round
 
     static Tuple2<Integer, StackAlias> pop(final StackAlias stack) {
         final Any<Integer> head = new AnyType<Integer>() {};
-        final Any<StackAlias> tail = new AnyType<StackAlias>() {};
+        //TODO: want to use StackAlias inside the Any here (see also the caseOf return val)
+        // but if you do, fails with: "io.soabase.halva.matcher.MatchError: No matches found and no default provided for: [3, 5, 8, 2, 1]"
+        final Any<ConsList<Integer>> tail = new AnyType<ConsList<Integer>>() {};
         return Matcher.match(stack)
                 //TODO: use new .caseOf method for this?
-                //TODO: this fails with: "io.soabase.halva.matcher.MatchError: No matches found and no default provided for: [3, 5, 8, 2, 1]"
-                .caseOf(Any.anyHeadAnyTail(head, tail), () -> Tuple.Tu(head.val(), tail.val()))
+                //TODO: doesn't seem to be type safe in the return value, eg change one of the types below and you get a runtime error
+                .caseOf(Any.anyHeadAnyTail(head, tail), () -> Tuple.Tu(head.val(), StackAlias.StackAlias(tail.val())))
                 .get();
     }
 
@@ -45,10 +52,64 @@ public class StateExample {
         return pop(new_stack_2._2);
     }
 
-    //@Test
+    @Test
     public void test_basicStack() {
         //TODO shouldn't I be able to assign this to a StackAlias?
+        // eg final StackAlias stack = List(5, 8, 2, 1); //gives compile error
         final ConsList<Integer> in_list = List(5, 8, 2, 1);
-        Assert.assertEquals(Tuple.Tu(4, List(8, 2, 1)), stackManip(StackAlias.StackAlias(in_list)));
+        Assert.assertEquals(Tuple.Tu(5, List(8, 2, 1)), stackManip(StackAlias.StackAlias(in_list)));
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    // 2) State based implementation of stack
+
+    static State<StackAlias, Integer> pop() {
+        return State.unit(s -> {
+            final Tuple2<Integer, StackAlias> s_i = pop(s);
+            //TODO: need a nicer PX<->TX utility...
+            return new P2<StackAlias, Integer>() {
+                @Override
+                public StackAlias _1() { return s_i._2; }
+                @Override
+                public Integer _2() { return s_i._1; }
+            };
+        });
+    }
+
+    static State<StackAlias, Unit> push(final Integer a) {
+        return State.unit(s -> {
+            final Tuple2<Unit, StackAlias> s_i = push(a, s);
+            //TODO: need a nicer PX<->TX utility...
+            return new P2<StackAlias, Unit>() {
+                @Override
+                public StackAlias _1() { return s_i._2; }
+                @Override
+                public Unit _2() { return s_i._1; }
+            };
+        });
+    }
+
+    static State<StackAlias, Integer> stackManip() {
+        final AnyVal<Unit> u = Any.make();
+        final AnyVal<Integer> a = Any.make();
+        final AnyVal<Integer> b = Any.make();
+        return StateFor.start()
+                .forComp(u, () -> push(3))
+                .forComp(a, () -> pop())
+                .forComp(b, () -> pop())
+                .yield(() -> b.val());
+    }
+
+    @Test
+    public void test_stateStack() {
+        //TODO shouldn't I be able to assign this to a StackAlias?
+        // eg final StackAlias stack = List(5, 8, 2, 1); //gives compile error
+        final ConsList<Integer> in_list = List(5, 8, 2, 1);
+        final P2<StackAlias, Integer> res = stackManip().run(StackAlias.StackAlias(in_list));
+        //TODO: need a nicer PX<->TX utility...
+        Assert.assertEquals(Tuple.Tu(List(8, 2, 1), 5), Tuple.Tu(res._1(), res._2()));
+        //(just double check original stack aka state hasn't changed:)
+        Assert.assertEquals(List(5, 8, 2, 1), in_list);
     }
 }
